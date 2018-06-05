116cf8c8a62fe08f111b626f615a129c28a13e6f
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-06-05 15:31:33 -0700

    Feat: add outlets to view controller

--- MiniGallery/ViewController.swift
+++ MiniGallery/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var moviesLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var swipeView: UIView!
+    @IBOutlet weak var scrollView: UIView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
